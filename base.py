1daa097f05ae80e1e57db7e84227476f2a237b27
Robson Barbiere <robertdev.mkt@gmail.com>
2025-06-17 22:09:11 -0300

    commit do desafio pronto

--- base.py
+++ base.py
@@ -18,19 +18,32 @@ while True:
     if entry == "d":
         value = float(input("Digite o valor do depósito: "))
         if value > 0:
-            ...
+            saldo += value
+            extrato += f"Depósito: R$ {value:.2f}\n"
+        else:
+            print("Operação falhou, digite um valor válido.")
     elif entry == "s":
         value = float(input("Digite o valor do saque: "))
         excedeu_limite = value > limite
         excedeu_saldo = value > saldo
-        excedeu_quantidade_saque = numero_saques > LIMITE_SAQUE
-
-        if value > 0:
-            ...
+        excedeu_quantidade_saque = numero_saques >= LIMITE_SAQUE
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif excedeu_limite:
+            print(f"Operação falhou! Limite de saque excedido você tem R$ {limite:.2f} por saque.")
+        elif excedeu_quantidade_saque:
+            print("Operação falhou! Limite diario de saque excedido.")
+        elif value > 0:
+            saldo -= value
+            extrato += f"Saque: R$ {value:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou! O valor informado é inválido.")
     elif entry == "e":
-        print(f"\n{" EXTRATO BANCÁRIO ".center(40, "=")}")
-
-        print("=" * 40, end="")
+        print(f"\n{" EXTRATO BANCÁRIO ".center(40, "=")}\n")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=" * 40)
     elif entry == "q":
         break
     else:
